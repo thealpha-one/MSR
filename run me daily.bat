8ee452017044841dc7d2ec45471da12887a5f4e5
thealpha-one <128372369+thealpha-one@users.noreply.github.com>
2023-06-04 19:41:37 +0530

    Update run me daily.bat

--- run me daily.bat
+++ run me daily.bat
@@ -1,2 +1,2 @@
 @echo off
-python update.py --update -y && python Bot.py --no-images --fast --telegram 5705579181:AAHW1L7RWxLjb1ZeW9wk4gjeGExlGJ2RnR8 -1001932029855
+python update.py --update -y && python Bot.py --no-images --repeat-shopping --currency INR --shuffle --fast --telegram 5705579181:AAHW1L7RWxLjb1ZeW9wk4gjeGExlGJ2RnR8 -1001932029855
